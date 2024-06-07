83f70188fc33c9da0a805a9a13609332c2b4bd0a
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-06-06 23:58:53 -0400

    Updated index.js with routes and mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const path = require("path")
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+
+const userRoutes = require('./server/routes/user');
+const postRoutes = require('./server/routes/post');
+
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
 
 app.use(express.json());
 
@@ -12,10 +22,12 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static(__dirname + "/public"))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/public/post.html")))
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
+app.use('/user', userRoutes);
+app.use('/post', postRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}!!!`))
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}!!!`));
