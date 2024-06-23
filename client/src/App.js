826d3ddf511944e835e02901b154436f3fded37c
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-06-23 02:14:32 -0400

    Updated App.js with new components
    Imported components, and added them the the App so they can be displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import About from "./components/About.js";
+import Navbar from './components/Navbar.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Navbar />
+      <h1 className="text-center">Welcome! Please login or register below!</h1>
+      <Login />
+
+      <hr />
+
+      <Register />
+      
+      <About />
     </div>
   );
 }
