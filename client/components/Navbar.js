71e53d9422b301fe2fa2e50153db7c747d146206
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-07-03 12:21:18 -0400

    New functionality for Navbar
    imported outlet and link
    replaced <a> with <link> in multiple places

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,21 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">FrankC App</a>
+                    <Link className="navbarbrand" to="/">About Me</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link" aria-current="page" href="#">Profile</a>
-                            <a className="nav-link" href="#">Login</a>
-                            <a className="nav-link" href="#">Register</a>
+                            <Link className="nav-link" to="/profile">Profile</Link>
+                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link className="nav-link" to="/register">Register</Link>
                         </div>
                     </div>
                 </div>
             </nav>
+            <Outlet />
     </div>
     );
 }
