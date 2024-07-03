3251249df1e8b6ba2292e6aa10fc9b498129d8d6
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-07-03 12:22:04 -0400

    Added login form functionality
    Same as Register, just using login paths

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,20 +1,76 @@
+import { fetchData } from "../../main";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const {username, email, password} = user;
+
+    const onChange = (e) => setUser({...user, [e.target.name]: e.target.value})
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        fetchData('/user/login', {
+            username,
+            password
+        }, 
+        "POST"
+        )
+        .then((data) => {
+            if(!data.message) {
+                navigate("/profile");
+            }                
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    }
+
     return(
         <div>
             <h2>Login Here!</h2>
-            <div classNameName="shadow p-3 mb-5 bg-body rounded">
-                <form>
+            <div className="shadow p-3 mb-5 bg-body rounded">
+                <form onSubmit={onSubmit}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email Address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1"></input>
+                        <label htmlFor="email" className="form-label">Email Address</label>
+                        <input type="text" 
+                        className="form-control" 
+                        id="email"
+                        name="email" 
+                        onChange={onChange} 
+                        value={email} 
+                        required></input>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputUsername1" className="form-label">Username</label>
-                        <input type="username" className="form-control" id="exampleInputUsername1"></input>
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input 
+                        type="text" 
+                        className="form-control" 
+                        id="username" 
+                        name="username" 
+                        onChange={onChange} 
+                        value={username} 
+                        required></input>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input 
+                        type="password" 
+                        className="form-control" 
+                        id="password" 
+                        name="password" 
+                        onChange={onChange} 
+                        value={password} 
+                        required></input>
                     </div>
                     <button type="submit" className="btn btn-primary">Login</button>
                 </form>
