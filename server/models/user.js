cd31f77e76bc41b01abaa366e71d79871189a8c2
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-07-03 12:00:10 -0400

    Removed first and last name attributes

--- server/models/user.js
+++ server/models/user.js
@@ -7,8 +7,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true},
   password: { type: String, required: true},
   email: { type: String, required: true},
-  first_name: { type: String},
-  last_name: { type: String},
   followers: [String],
   following: [String]
 })
