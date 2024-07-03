0de1e5d15a534393c8af9e8c415c6811d61f57d6
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2024-07-03 12:18:48 -0400

    Added functionality for form
    FetchData and useState
    onSubmit, onChange functions

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,37 +1,88 @@
+import { fetchData } from "../../main";
+import { useState} from "react";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const {username, email, password} = user;
+
+    const onChange = (e) => setUser({...user, [e.target.name]: e.target.value})
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // if(password.length <= 8) {
+        //     console.log("password must be greater than 8 characters!")
+        // } else {
+        //     console.log("Success!")
+        // }
+
+        fetchData('/user/register', {
+            username,
+            password
+        }, 
+        "POST"
+        )
+        .then((data) => {
+            if(!data.message) {
+                navigate("/profile");
+            }                
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+    }
+
     return(
         <div>
             <h2>Register Here!</h2>
-            <div classNameName="shadow p-3 mb-5 bg-body rounded">
-                <form>
+            <div className="shadow p-3 mb-5 bg-body rounded">
+                <form onSubmit={onSubmit}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email Address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                        <label htmlFor="email" className="form-label">Email Address</label>
+                        <input 
+                        type="text" 
+                        className="form-control" 
+                        id="email" 
+                        name="email" 
+                        onChange={onChange} 
+                        value={email}
+                        aria-describedby="emailHelp" 
+                        required ></input>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputUsername1" className="form-label">Username</label>
-                        <input type="username" className="form-control" id="exampleInputUsername1"></input>
+                        <label htmlFor="username" className="form-label">Username</label>
+                        <input 
+                        type="text" 
+                        className="form-control" 
+                        id="username" 
+                        name="username"
+                        onChange={onChange} 
+                        value={username}
+                        required></input>
                     </div>
                     <div className="mb-3">
-                        <label for="exampleInputFirst1" className="form-label">First Name</label>
-                        <input type="firstname" className="form-control" id="exampleInputFirst1" aria-describedby="optionalHelp"></input>
-                        <div id="optionalBlock" className="form-text text-info">
-                            Optional
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <label for="exampleInputLast1" className="form-label">Last Name</label>
-                        <input type="lastname" className="form-control" id="exampleInputLast1" aria-describedby="optionalHelp"></input>
-                        <div id="optionalBlock" className="form-text text-info">
-                            Optional
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" aria-describedby="passwordHelpBlock"></input>
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input 
+                        type="password" 
+                        className="form-control" 
+                        id="password" 
+                        name="password" 
+                        onChange={onChange} 
+                        value={password}
+                        aria-describedby="passwordHelpBlock" 
+                        required></input>
                         <div id="passwordHelpBlock" className="form-text">
-                            Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                            Your password must be greater than 8 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
                         </div>
                     </div>
                     <button type="submit" className="btn btn-primary">Register</button>
